4c4fd665a23c41b388e57edcffa989e2565868a6
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-04-17 06:51:24 +0500

    Hover Changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const getcolor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215)
     const randomCode = "#" +randomNumber.toString(16)
-    document.body.style.backgroundColor= randomCode;
+    document.body.style.backgroundColor = randomCode;
     document.getElementById("color-code").innerText = randomCode;
 
     navigator.clipboard.writeText(randomCode);
